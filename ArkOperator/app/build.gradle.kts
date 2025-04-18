7cb441eb7b732c73adabc66a980f42fdd61034de
Bastilicious <carlossebastianmiguel.bsit@gmail.com>
2025-04-18 16:11:26 +0800

    feat: ArkOperator - Add Device, Vessel List, Controller, Mic, Speaker, Bluetooth

--- ArkOperator/app/build.gradle.kts
+++ ArkOperator/app/build.gradle.kts
@@ -37,10 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.okhttp)
-    implementation(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
 }
